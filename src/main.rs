dc6d98d62d80e8bebdc3502eb2e38c02e432f507
watasuke102 <watasuke102@gmail.com>
2024-04-14 00:28:56 +0900

    update: read data from toml file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+// remind4f - main.rs
+//
+// CopyRight (c) 2024 Watasuke
+// Email  : <watasuke102@gmail.com>
+// Twitter: @Watasuke102
+// This software is released under the MIT or MIT SUSHI-WARE License.
+use serde::Deserialize;
+use std::io::ErrorKind;
+
+#[derive(Deserialize, Debug)]
+struct Data {
+  settings: Settings,
+  events:   Vec<Event>,
+}
+#[derive(Deserialize, Debug)]
+struct Settings {
+  discord_bot_token: String,
+  channel_id:        String,
+  disable_everyone:  bool,
+  notice_time:       String,
+}
+#[derive(Deserialize, Debug)]
+struct Event {
+  title: String,
+  desc:  String,
+  date:  String,
+}
+
 fn main() {
-    println!("Hello, world!");
+  let data: Data = match &std::fs::read_to_string("data.toml") {
+    Ok(s) => toml::from_str(s).unwrap(),
+    Err(e) => {
+      if e.kind() == ErrorKind::NotFound {
+        panic!(
+          "`data.toml` is not found. Try `cp data-sample.toml data.toml`\n({})",
+          e
+        );
+      }
+      panic!("{}", e);
+    }
+  };
+  println!("{:#?}", data);
 }
